4ae42db62c08e98ad4591660cfe47e9d4aff9c58
sharfine <784751530@qq.com>
2020-06-08 18:19:57 +0800

    :spark: modify

--- eureka-consumer-feign/src/main/java/com/sharfine/springcloud/client/HelloClient.java
+++ eureka-consumer-feign/src/main/java/com/sharfine/springcloud/client/HelloClient.java
@@ -1,10 +1,11 @@
 package com.sharfine.springcloud.client;
 
+import com.sharfine.springcloud.client.fallbackFactory.HelloFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "eureka-producer")
+@FeignClient(name = "eureka-producer", fallbackFactory = HelloFallbackFactory.class)
 public interface HelloClient {
     @GetMapping("/hello/")
     String hello(@RequestParam(value = "name") String name);
---
+++ eureka-consumer-feign/src/main/java/com/sharfine/springcloud/client/fallbackFactory/HelloFallbackFactory.java
@@ -0,0 +1,19 @@
+package com.sharfine.springcloud.client.fallbackFactory;
+
+import com.sharfine.springcloud.client.HelloClient;
+import feign.hystrix.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloFallbackFactory implements FallbackFactory<HelloClient> {
+
+    @Override
+    public HelloClient create(Throwable throwable) {
+        return new HelloClient() {
+            @Override
+            public String hello(String name) {
+                return "熔断了";
+            }
+        };
+    }
+}
