ba61fc62843ed7952ee84e9cd09536dcdad22448
sharfine <784751530@qq.com>
2020-06-10 18:07:00 +0800

    :sparkles: add gateway filter

--- gateway/src/main/java/com/sharfine/springcloud/GatewayApplication.java
+++ gateway/src/main/java/com/sharfine/springcloud/GatewayApplication.java
@@ -1,13 +1,39 @@
 package com.sharfine.springcloud;
 
+import com.sharfine.springcloud.filter.ElapsedFilter;
+import com.sharfine.springcloud.filter.TokenFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+
     }
 
+    @Bean
+    public TokenFilter tokenFilter() {
+        return new TokenFilter();
+    }
+
+    @Bean
+    public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
+        // @formatter:off
+        return builder.routes()
+                .route(r -> r.path("/fluent/eureka-consumer-feign/**")
+                        .filters(f -> f.stripPrefix(2)
+                                .filter(new ElapsedFilter())
+                                .addResponseHeader("X-Response-Default-Foo", "Default-Bar"))
+                        .uri("lb://EUREKA-CONSUMER-FEIGN")
+                        .order(0)
+                        .id("fluent_customer_service")
+                )
+                .build();
+        // @formatter:on
+    }
 }
